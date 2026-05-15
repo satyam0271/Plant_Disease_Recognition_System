cf1360346a6d3574bd13b59bc7309e02c96c574b
satyam <satyamkumar7912@gmail.com>
2026-05-15 17:02:48 +0530

    updated app.py

--- app.py
+++ app.py
@@ -120,4 +120,5 @@ def history():
     return render_template('history.html', recent=recent_predictions)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
